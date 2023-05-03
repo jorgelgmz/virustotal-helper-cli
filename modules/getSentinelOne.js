4d923c8917835f088b5dcdc24341b7f60705ad3f
Jorge L Gomez <jorgelgmz@gmail.com>
2023-05-03 13:01:58 -0400

    Added name to VT script

--- modules/getSentinelOne.js
+++ modules/getSentinelOne.js
@@ -7,7 +7,7 @@ const wait = (ms) => new Promise((res) => setTimeout(res, ms));
 
 const getSentinelOne = async (api) => {
   const hashes = (await fs.readFile(path.join(process.cwd(), 'hashes.txt'), 'utf-8')).split(/\n\r?/);
-  await fs.appendFile(path.join(process.cwd(), 'output.csv'), `Category,Engine Name,Engine Version,Result,Method,Engine Update\n`, 'utf-8');
+  await fs.appendFile(path.join(process.cwd(), 'output.csv'), `Name,Category,Engine Name,Engine Version,Result,Method,Engine Update\n`, 'utf-8');
   for (let hash of hashes) {
     try {
       let scanners = await axios.get(`https://www.virustotal.com/api/v3/files/${hash}`, {
@@ -17,7 +17,11 @@ const getSentinelOne = async (api) => {
       if (await scanners.SentinelOne) {
         await fs.appendFile(
           path.join(process.cwd(), 'output.csv'),
-          `${scanners.SentinelOne.category},${scanners.SentinelOne.engine_name},${scanners.SentinelOne.engine_version},${scanners.SentinelOne.result},${scanners.SentinelOne.method},${scanners.SentinelOne.engine_update}\n`,
+          `${response.data.data.attributes.names.toString().replaceAll(',', ';')},${scanners.SentinelOne.category},${
+            scanners.SentinelOne.engine_name
+          },${scanners.SentinelOne.engine_version},${scanners.SentinelOne.result},${scanners.SentinelOne.method},${
+            scanners.SentinelOne.engine_update
+          }\n`,
         );
         console.log(chalk.bold.blue('Success:'), 'SentinelOne detection for', chalk.bold.blue(hash), 'found. Result sent to output.csv');
       } else {
